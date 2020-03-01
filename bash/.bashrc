71c547022d6fa22dc68879f5c370647db3d750a5
Josh Trahan <josht0288@gmail.com>
2020-02-29 22:07:49 -0600

    add script for setting up in containers

--- bash/.bashrc
+++ bash/.bashrc
@@ -2,6 +2,10 @@
 # see /usr/share/doc/bash/examples/startup-files (in the package bash-doc)
 # for examples
 
+if [ -e ${HOME}/.bashrc.existing ]; then
+    source ${HOME}/.bashrc.existing
+fi
+
 # If not running interactively, don't do anything
 case $- in
     *i*) ;;
---
+++ container_setup.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if ! [ -x "$(command -v stow)" ]; then
+    echo "GNU Stow not found. Aborting."
+    exit 1
+fi
+
+dotfiles_dir=$(dirname $(readlink -f $0))
+target_dir=${HOME}
+
+if [ -e ${HOME}/.bashrc ]; then
+    mv ${HOME}/.bashrc ${HOME}/.bashrc.existing
+fi
+
+stow -d $dotfiles_dir -t $target_dir bash
+stow -d $dotfiles_dir -t $target_dir tmux
