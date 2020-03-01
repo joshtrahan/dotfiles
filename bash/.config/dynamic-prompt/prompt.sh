c0f6af466fabc4457c4659cd1a2da8e90acc2470
Josh Trahan <josht0288@gmail.com>
2020-02-29 21:28:03 -0600

    add exit status to prompt

--- bash/.config/dynamic-prompt/prompt.sh
+++ bash/.config/dynamic-prompt/prompt.sh
@@ -1,8 +1,20 @@
 source ~/.config/git-aware-prompt/prompt.sh
 source ~/.config/dynamic-prompt/determine_color.sh
 
+exit_status() {
+    local exit_status=$?
+
+    if [ $exit_status = '0' ]; then
+        local exit_color="${txtgrn}"
+    else
+        local exit_color="${bakred}${txtblk}"
+    fi
+
+    echo "${exit_color}${exit_status}${txtrst}"
+}
+
 generate_prompt() {
-    export PS1="${txtcyn}\u@\h${txtrst}:${bldpur}$(pwd) ${bldcyn}$(find_git_branch)$(find_git_dirty) ${txtrst}$(date) \n$ "
+    export PS1="${txtcyn}\u@\h${txtrst}:${bldpur}\w $(exit_status) ${bldcyn}$(find_git_branch)$(find_git_dirty) ${txtrst}$(date) \n$ "
 }
 
 PROMPT_COMMAND='generate_prompt'
