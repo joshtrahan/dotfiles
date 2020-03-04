258f54b393a6cb1e073aed62110ddf93387b19eb
Josh Trahan <josht0288@gmail.com>
2020-03-03 18:22:27 -0600

    rewrite container setup script

--- container_setup.sh
+++ container_setup.sh
@@ -1,26 +1,16 @@
 #!/bin/sh
 
+
 if ! [ -x "$(command -v stow)" ]; then
-    apt-get update
-    apt-get install -y stow
+    echo "source ${HOME}/.dotfiles/bash/.bashrc" > ${HOME}/.bashrc
+else
+    dotfiles_dir=$(dirname $(readlink -f $0))
+    target_dir=${HOME}
+
+    if [ -e ${HOME}/.bashrc ]; then
+        mv ${HOME}/.bashrc ${HOME}/.bashrc.container
+    fi
+
+    stow -d $dotfiles_dir -t $target_dir bash
+    stow -d $dotfiles_dir -t $target_dir tmux
 fi
-if ! [ -x "$(command -v stow)" ]; then
-    echo "GNU Stow not found. Aborting."
-    exit 1
-fi
-
-dotfiles_dir=$(dirname $(readlink -f $0))
-target_dir=${HOME}
-
-workdir=$(pwd)
-cd $dotfiles_dir
-git submodule init
-git submodule sync
-cd $workdir
-
-if [ -e ${HOME}/.bashrc ]; then
-    mv ${HOME}/.bashrc ${HOME}/.bashrc.container
-fi
-
-stow -d $dotfiles_dir -t $target_dir bash
-stow -d $dotfiles_dir -t $target_dir tmux
